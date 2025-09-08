95069d0e50ce4c5c23e3ea7cd4df8ac8396d367c
SM Anas <smanas.net@gmail.com>
2025-09-08 17:38:15 +0600

    Switches payment gateway to SSL Commerz
    Removes PayPal payment initiation logic and associated UI elements from the checkout process. Integrates SSL Commerz as the primary payment method.
    
    Updates the API endpoint in the environment configuration to point to the deployed server.

--- src/pages/shopping-view/checkout.jsx
+++ src/pages/shopping-view/checkout.jsx
@@ -34,70 +34,6 @@ function ShoppingCheckout() {
         )
       : 0;
 
-  function handleInitiatePaypalPayment() {
-    if (cartItems.length === 0) {
-      toast({
-        title: "Your cart is empty. Please add items to proceed",
-        variant: "destructive",
-      });
-
-      return;
-    }
-    if (currentSelectedAddress === null) {
-      toast({
-        title: "Please select one address to proceed.",
-        variant: "destructive",
-      });
-
-      return;
-    }
-
-    const orderData = {
-      userId: user?.id,
-      cartId: cartItems?._id,
-      cartItems: cartItems.items.map((singleCartItem) => ({
-        productId: singleCartItem?.productId,
-        title: singleCartItem?.title,
-        image: singleCartItem?.image,
-        price:
-          singleCartItem?.salePrice > 0
-            ? singleCartItem?.salePrice
-            : singleCartItem?.price,
-        quantity: singleCartItem?.quantity,
-      })),
-      addressInfo: {
-        addressId: currentSelectedAddress?._id,
-        address: currentSelectedAddress?.address,
-        city: currentSelectedAddress?.city,
-        pincode: currentSelectedAddress?.pincode,
-        phone: currentSelectedAddress?.phone,
-        notes: currentSelectedAddress?.notes,
-      },
-      orderStatus: "pending",
-      paymentMethod: "sslcommerz",
-      paymentStatus: "pending",
-      totalAmount: totalCartAmount,
-      orderDate: new Date(),
-      orderUpdateDate: new Date(),
-      paymentId: "",
-      payerId: "",
-    };
-
-    dispatch(createNewOrder(orderData)).then((data) => {
-      console.log(data, "sangam");
-      window.location.replace(data?.payload);
-      // if (data?.payload?.success) {
-      //   setIsPaymemntStart(true);
-      // } else {
-      //   setIsPaymemntStart(false);
-      // }
-    });
-  }
-
-  if (approvalURL) {
-    window.location.href = approvalURL;
-  }
-
   return (
     <div className="flex flex-col">
       <div className="relative h-[300px] w-full overflow-hidden">
@@ -121,11 +57,7 @@ function ShoppingCheckout() {
             </div>
           </div>
           <div className="mt-4 w-full">
-            <Button onClick={handleInitiatePaypalPayment} className="w-full">
-              {isPaymentStart
-                ? "Processing Paypal Payment..."
-                : "Checkout with Paypal"}
-            </Button>
+            <Button className="w-full">Pay With SSL Commerz</Button>
           </div>
         </div>
       </div>
